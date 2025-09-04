a5cbb5a928269a29c1c1700745b307340df8ab7e
AliaksandrParetski <paretskialiaksandr@gmail.com>
2025-09-04 10:54:56 +0300

    feat: create bootstrapper object.
    create Boot scene, Bootstrapper object, Bootstrapper.cs

---
+++ Assets/Scripts/Core/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace HCT.Scripts.Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Game started!");
+        }
+    }
+}
