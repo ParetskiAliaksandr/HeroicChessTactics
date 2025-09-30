3ea413d5e6f59f3c25d60d1348ff0375054af7b7
AliaksandrParetski <paretskialiaksandr@gmail.com>
2025-09-30 12:24:05 +0300

    refactor(scene-flow): move mapping key→scene name to ISceneConfigProvider

---
+++ Assets/Scripts/Services/ConfigManagement/Providers/SceneConfigProvider.cs
@@ -0,0 +1,20 @@
+﻿
+using HCT.Scripts.Enums;
+
+namespace HCT.Scripts.Services.ConfigManagement.Providers
+{
+    public class SceneConfigProvider : ISceneConfigProvider
+    {
+        private readonly SceneConfigSO _sceneConfigSO;
+
+        public SceneConfigProvider(ISceneConfigeProvader configService)
+        {
+            _sceneConfigSO = configService.GameConfig.SceneConfigSO;
+        }
+
+        public string GetSceneName(SceneKey sceneKey)
+        {
+            return _sceneConfigSO.GetSceneName(sceneKey);
+        }
+    }
+}
